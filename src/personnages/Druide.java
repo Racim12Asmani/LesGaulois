66398cf451c4d556db545ea51c202c53ffa9431f
Racim12Asmani <racim283@gmail.com>
2025-11-03 20:01:53 +0100

    Complétion point 4 tp1 poo

---
+++ src/personnages/Druide.java
@@ -0,0 +1,32 @@
+package personnages;
+
+public class Druide {
+
+    private String nom;
+    private int force;
+
+    public Druide(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
+
+    private String prendreParole() {
+        return "Le druide " + nom + " : ";
+    }
+
+    public void fabriquerPotion(int quantite, int forcePotion) {
+    }
+
+
+    public void booster(Gaulois gaulois) {
+
+    }
+}
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,40 +1,44 @@
 package personnages;
 
 public class Gaulois {
-    private String nom;
-    private int force;
+    private String nom;          
+    private int force;           
+    private int effetPotion = 1; 
 
     public Gaulois(String nom, int force) {
         this.nom = nom;
         this.force = force;
     }
 
+
     public String getNom() {
         return nom;
     }
+
 
     public void parler(String texte) {
         System.out.println(prendreParole() + "\"" + texte + "\"");
     }
 
+
     private String prendreParole() {
-        return "Le Gaulois " + nom + " : ";
+        return "Le gaulois " + nom + " : ";
     }
-    
-    public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
-		System.out.println(asterix);
-	}
-
-	@Override
-	public String toString() {
-		return nom;
-	}
-	
-	public void frapper(Romain romain) {
-	    System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-	    romain.recevoirCoup(force / 3);
-	}
 
 
+    @Override
+    public String toString() {
+        return nom;
+    }
+
+
+    public void frapper(Romain romain) {
+        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+        romain.recevoirCoup(force / 3);
+    }
+
+
+    public void boirePotion(int forcePotion) {
+        effetPotion = forcePotion;
+    }
 }
